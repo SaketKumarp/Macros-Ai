2dee218d72b11bd958ed60e519896ae09c75af64
Saket Kumar <madmansaketkumar@gmail.com>
2026-04-12 23:32:06 +0530

    added sign-out

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,28 @@
-import { useUser } from "@clerk/expo";
+import { Button } from "@/components/ui/button";
+import { useClerk, useUser } from "@clerk/expo";
 import { Link, Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   const { isSignedIn, user } = useUser();
+  const { signOut } = useClerk();
+  const handleSignout = async () => {
+    try {
+      await signOut();
+    } catch (err) {
+      console.log("error in sign-out", err);
+    }
+  };
+
   if (!isSignedIn) {
     return <Redirect href={"/(auth)/sign-up"} />;
   }
   return (
     <View className="bg-red-300 flex min-h-screen justify-center items-center">
-      <Link href={"/sign-up"}>
-        <Text>welcome !{user?.fullName}</Text>
-      </Link>
+      <Text>welcome !{user?.fullName}</Text>
+      <Button variant={"outline"} onPress={handleSignout}>
+        <Text>Sign-Out</Text>
+      </Button>
     </View>
   );
 }
